c55e039f8377d694c6ad9bf57fd16e38f422ad16
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2024-02-29 16:43:29 +0300

    Add graphql mutation

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
-import { GET_USERS } from '../../graphql.operations';
+import { GET_USERS , CREATE_USER } from '../../graphql.operations';
+import { ExecutionResult } from 'graphql';
+import { ApolloError } from '@apollo/client';
 
 @Component({
   selector: 'app-signup',
@@ -14,15 +16,38 @@ export class SignupComponent implements OnInit {
   constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
-    this.apollo.watchQuery({
-      query: GET_USERS
-    }).valueChanges.subscribe(({ data, error }: any) => {
-      if (data && data.users && data.users.edges) {
-        this.users = data.users.edges.map((edge: any) => edge.node);
+  this.apollo.watchQuery({
+    query: GET_USERS
+  }).valueChanges.subscribe(({ data, error }: any) => {
+    if (data && data.users && data.users.edges) {
+      this.users = data.users.edges.map((edge: any) => edge.node);
+    }
+    this.error = error;
+  });
+
+  this.apollo.mutate({
+    mutation: CREATE_USER,
+    variables: {
+      input: {
+        // Remplacez ces valeurs par celles que vous souhaitez créer
+        email: '',
+        firstName: '',
+        gender: '',
+        id: '',
+        isActive: false,
       }
-      this.error = error;
-    });
-  }
-  
+    }
+  }).subscribe((result: ExecutionResult<any>) => {
+    // Vérifier si 'data' est défini et a le bon format
+    if (result.data && result.data.createUser) {
+      // Gérer la réponse de la mutation ici
+      console.log('Utilisateur créé : ', result.data.createUser);
+    }
+  }, (error: ApolloError) => {
+    // Gérer les erreurs éventuelles ici
+    console.error('Erreur lors de la création de l\'utilisateur : ', error);
+  });
+}
+
 
 }
--- src/app/graphql.operations.ts
+++ src/app/graphql.operations.ts
@@ -2,18 +2,54 @@ import { gql } from "apollo-angular";
 
 const GET_USERS = gql `
     query Users {
-        users(first: 4) {
+        users(first: 10) {
             edges {
                 node {
+                    username
                     email
-                    firstName
                     gender
-                    id
-                    isActive
+                    location
+                    website
+                    bio
+                    interests
+                    phoneNumber
                 }
+            }
+            pageInfo{
+                endCursor
+                startCursor
+                hasNextPage
+                hasPreviousPage
             }
         }
     }
-`
+`;
 
 export { GET_USERS };
+
+export const CREATE_USER = gql`
+mutation CreateUser {
+    createUser(
+      input: {
+        username: "Ariel", 
+        email: "ariel@gmail.com", 
+        gender: "Female", 
+        location: "Antananarivo", 
+        bio: "Lorem ipsum dolor sit amet", 
+        interests: "Dance", 
+        phoneNumber: "347716184", 
+        password: "ariel@1234"}
+    ) {
+      user {
+        username
+        email
+        gender
+        location
+        website
+        bio
+        interests
+        phoneNumber
+      }
+    }
+  }
+`;
